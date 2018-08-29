5cfc6a15e9b851f0eee0ce3806b3a4bc292f36ea
Paulke29 <34780166+Paulke29@users.noreply.github.com>
2018-08-29 14:55:22 -0700

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,40 @@
+public class Arithmetic{
+	int a = 16;
+	int b = 4;
+	float x =4;
+	float y = 1;
+	int result1;
+	float result2;
+	// public Arithmetic(int a,int b,float x,float y){
+	// 	this.a = a;
+	// 	this.b = b;
+	// 	this.x = x;
+	// 	this.y = y;
+	// };
+
+	// public int Addition(int a,int b){
+	// 	result1 = a + b;
+	// 	return result1;
+
+	// }
+	// public float Division (float x, float y){
+	// 	result2 = x /y;
+	// 	return result2;
+	// }
+	public static void main(String []args){
+		// Arithmetic arithmetic = new Arithmetic(16,4,4,1);
+		int a = 16;
+	int b = 4;
+	float x =4;
+	float y = 1;
+	int result1;
+	float result2 = x + y;
+		int c = a + b;
+
+		System.out.println("the result of addtion is"+c);
+		System.out.println("the result of Division is"+result2);
+
+	}
+	
+
+}
